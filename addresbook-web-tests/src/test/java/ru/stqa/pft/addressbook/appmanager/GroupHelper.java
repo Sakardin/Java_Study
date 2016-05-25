6017bf65b8a26cfaa0daacb5da912e048d3838ba
Dmitry Sakardin <dsakardin@tula.co>
2016-05-25 17:50:53 +0300

    Кеширование

--- addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -66,26 +66,30 @@ public class GroupHelper  extends HeplerBase {
 
     }
 
+    private Groups groupCache = null;
+
 
     public Groups all() {
-        Groups groups = new Groups();
+        if(groupCache != null){
+            return new Groups(groupCache);
+        }
+        groupCache = new Groups();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements){
             String name = element.getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            groups.add(new GroupData().withId(id).withName(name));
+            groupCache.add(new GroupData().withId(id).withName(name));
         }
 
-        return groups;
+        return new Groups(groupCache);
     }
 
     public void modify(GroupData group) {
         selectGroupById(group.getId());
         initGroupModification();
-
         fillGroupForm(group);
-
         submitGroupModification();
+        groupCache = null;
         returnToGroupPage();
     }
     public void delete(int index) {
@@ -98,12 +102,15 @@ public class GroupHelper  extends HeplerBase {
 
         fillGroupForm(group);
         submitGroupCreation();
+        groupCache = null;
+
         returnToGroupPage();
     }
 
     public void delete(GroupData group) {
         selectGroupById(group.getId());
         deleteSelectedGroup();
+        groupCache = null;
         returnToGroupPage();
 
     }
